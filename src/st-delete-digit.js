fe46a170c9eb8645fc0ee268dbaa9da5ff0db79a
Dmitry Buravkin <dmitry.buravkin@gmail.com>
2021-10-02 10:37:48 +0300

    feat: add deleteigit method

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  let arr = String(n).split('').map(el => +el);
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let newArr = arr.slice();
+    newArr.splice(i, 1);
+    result = Math.max(result, newArr.join(''));
+  }
+
+  return result;
 }
